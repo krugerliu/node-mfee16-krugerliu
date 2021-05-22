a0ff907ac134ec47df42cd1275a994f3f5443c2d
rogerliu <krugerliu@hotmail.com>
2021-05-22 16:19:46 +0800

    add sum.js

---
+++ sum.js
@@ -0,0 +1,16 @@
+console.log("hello, this is roger's web site!");
+
+
+var i;
+var j=0;
+
+myFunction(10);
+
+function myFunction(p1) {
+    for (i = 1; i <= p1; i++) {
+        j=j+i;
+        i++;
+    };
+    console.log(j);
+};
+
