df947c4179d436826fe61779785bef985d8c52ef
krugerliu <krugerliu@hotmail.com>
2021-06-13 09:54:25 +0800

    update crawler-app.js

--- crawler/crawler-app.js
+++ crawler/crawler-app.js
@@ -24,27 +24,17 @@ connection = Promise.promisifyAll(connection);
             let result = await connection.queryAsync(`SELECT stock_id FROM stock WHERE stock_id = ${v_stockCode[i]}`);
             if (result.length <= 0) {
                 let response_stockCode = await axios.get(`https://www.twse.com.tw/zh/api/codeQuery?query=${v_stockCode[i]}`);
-                checkNewStock(response_stockCode.data.suggestions[0]);
+                checkNewStock(response_stockCode.data.suggestions[0]);      // 檢查證交所是否有此股票代碼
             };
             let response = await axios.get(
                 "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
-                {
-                    params: {
-                    response: "json",
-                    date: sys_date().format("YYYYMMDD"),
-                    stockNo: v_stockCode[i],
-                    },
-                }
+                {params: {response: "json", date: sys_date().format("YYYYMMDD"), stockNo: v_stockCode[i]}}
             );
-            if (response.data.stat !== "OK") {throw `證交所查無此股票代碼: ${v_stockCode[i]} \n`;};
+            if (response.data.stat !== "OK") {throw `證交所無此股票代碼: ${v_stockCode[i]} \n`;}; //無此股票代碼時,立即 throw exception 中斷往下執行
             console.log(response.data.title);
             let stockData = response.data.data;
             let stockTransaction = response.data.data.map((stockData) => {
-                stockData = stockData.map((value) => {return value.replace(/,/g, "");});
-                stockData[0] = parseInt(stockData[0].replace(/\//g, ""), 10) + 19110000;
-                stockData[0] = sys_date(stockData[0], "YYYYMMDD").format("YYYY-MM-DD");
-                stockData.unshift(v_stockCode[i]);
-                console.log(`交易日期 : ` + stockData[1] + `   收盤價格 : ` + stockData[6]);
+                stockData = stockData_reFormat(stockData, v_stockCode[i]);  // 資料重新校正回歸
                 return stockData;
             });
             await connection.queryAsync(`delete from stock_price where stock_id=${v_stockCode[i]}`);
@@ -64,4 +54,13 @@ function checkNewStock(resData){
     if (resDataSplit.length > 1) {
         connection.queryAsync(`INSERT INTO stock (stock_id, stock_name) VALUES ('${resDataSplit[0]}', '${resDataSplit[1]}');`);
     };
+};
+
+function stockData_reFormat(stockData, v_stockCode){
+    stockData = stockData.map((value) => {return value.replace(/,/g, "");});
+    stockData[0] = parseInt(stockData[0].replace(/\//g, ""), 10) + 19110000;
+    stockData[0] = sys_date(stockData[0], "YYYYMMDD").format("YYYY-MM-DD");
+    stockData.unshift(v_stockCode);
+    console.log(`交易日期 : ` + stockData[1] + `   收盤價格 : ` + stockData[6]);
+    return stockData;
 };
